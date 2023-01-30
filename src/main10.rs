32d19f26cb3bee77ce6981ca46fbe2ac78742cc2
RickKoenig <mkoeni@sbcglobal.net>
2023-01-29 19:39:08 -0800

    get largest working

--- src/main10.rs
+++ src/main10.rs
@@ -1,6 +1,9 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+use std::fmt::Debug;
+use std::fmt::Display;
+
 fn largest_int(list: &[i32]) -> i32 {
     let mut largest = list[0];
 
@@ -22,8 +25,8 @@ fn largest_char(list: &[char]) -> char {
     }
     largest
 }
-/*
-fn largest<T>(list: &[T]) -> T {
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
@@ -34,20 +37,22 @@ fn largest<T>(list: &[T]) -> T {
     }
     largest
 }
-*/
+
 pub fn main10() {
     {
-        let int_list = vec![34, 50, 25, 100, 65];
-        let result = largest_int(&int_list);
+        let i32_list = vec![34, 50, 25, 100, 65];
+        let result = largest_int(&i32_list);
         println!("The largest i32 is {}", result);
 
         let char_list = vec!['h', 'e', 'w', 'w', 'o', 'a'];
         let result = largest_char(&char_list);
         println!("The largest char is {}", result);
 
-        let number_list = vec![33, 55, 44, 22, 11];
-        //let result = largest(&number_list);
-        //println!("The largest int is {}", result);
+        let int_list = vec![33, 55, 44, 22, 11];
+        let result = largest(&int_list);
+        println!("The largest int is {}", result);
+
+        
     }
 
     {
@@ -119,9 +124,16 @@ pub fn main10() {
         trait Summary {
             //fn summarize(&self) -> String;
 
+            //fn summarize_author(&self) ->String;
+
             fn summarize(&self) -> String {
                 String::from("(Read more...)")
-            } 
+            }
+            /*
+            fn summarize(&self) -> String {
+                format!("(Read more from {}...)",
+                    self.summarize_author())
+            } */
         }
 
         struct NewsArticle {
@@ -144,13 +156,81 @@ pub fn main10() {
             retweet: bool,
         }
 
+        fn returns_summarizable() -> impl Summary {
+            Tweet {
+                username: String::from("horse_ebooks"),
+                content: String::from("of course, as you probably already know, people"),
+                reply: false,
+                retweet: false,
+            }
+        }
+
+        /* won't work, can't return multiple types
+        fn returns_summarizable2(switch: bool) -> impl Summary {
+            if switch {
+                NewsArticle {
+                    headline: String::from("Penguins win the Stanley Cup Championship!"),
+                    location: String::from("Pittsburgh, PA, USA"),
+                    author: String::from("Iceburgh"),
+                    content: String::from(
+                        "The Pittsburgh Penguins once again are the best
+                    hockey team in the NHL.",
+                    ),
+                }
+            } else {
+                Tweet {
+                    username: String::from("horse_ebooks"),
+                    content: String::from("of course, as you probably already know, people"),
+                    reply: false,
+                    retweet: false,
+                }
+            }
+        }
+        */
+
         impl Summary for Tweet {
-            /*
             fn summarize(&self) -> String {
                 format!("{}: {}", self.username, self.content)
-            } 
-            */
+            }
         }
+
+        struct Face {
+            user: String,
+            what: String,
+        }
+
+        impl Summary for Face {
+            /*fn summarize(&self) -> String {
+                format!("{}: {}", self.user, self.what)
+            }*/
+        }
+        /*
+                fn notify(item: impl Summary) {
+                    println!("Breaking news! sugar {}", item.summarize());
+                }
+        */
+
+        fn notify<T: Summary>(item: T) {
+            println!("Breaking news! no sugar {}", item.summarize());
+        }
+
+        fn notify2(item1: impl Summary, item2: impl Summary) {}
+        fn notify3<T: Summary>(item1: T, item2: T) {}
+
+        fn notify4(item: impl Summary + Display) {}
+
+        fn some_function<T: Display + Clone, U: Clone + Debug>(t: T, u: U) -> i32 {
+            34
+        }
+
+        fn some_function2<T, U>(t: T, u: U) -> i32
+        where
+            T: Display + Clone,
+            U: Clone + Debug,
+        {
+            444
+        }
+
         let tweet = Tweet {
             username: String::from("horse_ebooks"),
             content: String::from("of course, as you probably already know, people"),
@@ -163,7 +243,7 @@ pub fn main10() {
             headline: String::from("news"),
             location: String::from("loc"),
             author: String::from("auth"),
-            content: String::from("contents")
+            content: String::from("contents"),
         };
         println!("1 new newsArticle: {}", news_article.summarize());
 
@@ -171,9 +251,22 @@ pub fn main10() {
             headline: String::from("Penguins win the Stanley Cup Championship!"),
             location: String::from("Pittsburgh, PA, USA"),
             author: String::from("Iceburgh"),
-            content: String::from("The Pittsburgh Penguins once again are the best
-            hockey team in the NHL."),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best
+            hockey team in the NHL.",
+            ),
         };
         println!("New article available! {}", article.summarize());
+
+        let face = Face {
+            user: String::from("myself"),
+            what: String::from("news from face"),
+        };
+
+        println!("New face! {}", face.summarize());
+
+        notify(tweet);
+        notify(article);
+        notify(face);
     }
 }
